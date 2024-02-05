e657981455f984854624bff0ad77dbaed0e98088
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-02-05 13:26:27 +0000

    m

--- include/status.php
+++ include/status.php
@@ -155,8 +155,8 @@ if ($check_logics[0] == "SimplexLogic") {
    echo "</div></td></tr>";
    }
    echo "<table style=\"margin-top:4px;margin-bottom:13px;\"><tr><th colspan=2 >Editing</th></tr><tr>";
-   echo "<td colspan=2 style=\"background:#ffffed;\"><div style=\"margin-top:4px;margin-bottom:4px;white-space:normal;color:#000000;font-weight: bold;\">"; 
-   echo "Last Reboot<br>",exec('uptime -s');
+   //echo "<td colspan=2 style=\"background:#ffffed;\"><div style=\"margin-top:4px;margin-bottom:4px;white-space:normal;color:#000000;font-weight: bold;\">"; 
+   //echo "Last Reboot<br>",exec('uptime -s');
    echo "</div></td></tr>";
 if ($_SESSION['auth'] === "UNAUTHORISED") {
   echo "<td colspan=2 style=\"background:#ffffed;\"><div style=\"margin-top:4px;margin-bottom:4px;white-space:normal;color:#0a7d29;font-weight: bold;\">";
