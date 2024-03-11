f7836a0a5bff704b9273e22a1274466ff2a770df
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-03-11 11:17:29 +0000

    sync

---
+++ include/test.php
@@ -0,0 +1,14 @@
+<?php
+include 'site_config.php';
+echo DIR_BASE;
+echo DIR_CLASSES;
+echo DIR_SITE;
+echo DIR_SITE_VIEWS;
+echo DIR_SITE_COMPONENTS;
+echo BASE_URL;
+echo SITE_URL;
+echo DIR_STATIC;
+echo DIR_JS;
+echo DIR_CSS;
+echo DIR_SITE_IMG;
+?>
