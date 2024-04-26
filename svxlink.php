19629847d9957702ca113e54e2b971f0b2db3921
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-04-26 13:19:19 +0100

    m

--- svxlink.php
+++ svxlink.php
@@ -1,7 +1,6 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-    if ($_SESSION['auth'] == 'AUTHORISED')
-    session_start();} else if ($_SESSION['auth'] == '') {  
+    session_start();
 }
 include "include/settings.php";
 
