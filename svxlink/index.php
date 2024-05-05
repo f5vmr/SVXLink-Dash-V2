acef4cf04f61a0403fb42c28bdfef73384b389dc
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-05 10:44:14 +0100

    m

--- svxlink/index.php
+++ svxlink/index.php
@@ -224,10 +224,10 @@ textarea {
 
 
         $ini = build_ini_string($svxconfig);
-        var_dump($ini);
+        
 
         //file_put_contents("/var/www/html/test.ini",$ini,FILE_USE_INCLUDE_PAT);
-        file_put_contents("/var/www/html/svxlink/svxlink.conf", $ini ,FILE_USE_INCLUDE_PATH);
+        file_put_contents("/etc/svxlink/svxlink.conf", $ini ,FILE_USE_INCLUDE_PATH);
 
 	///file manipulation section
 
