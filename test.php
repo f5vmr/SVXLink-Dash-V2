23c07833af20a932a11c68b6ad5c2411ef71a367
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-05 14:52:52 +0100

    m

--- test.php
+++ test.php
@@ -1,29 +1,16 @@
 <?php
 session_start();
-$svxConfigFile = "/etc/svxlink/svxlink.conf";
-$svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
-$callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
-// check if we are a repeater or a simplex system
-$check_logics = explode(",",$svxconfig['GLOBAL']['LOGICS']);
-foreach ($check_logics as $logic_key) {
-    if ($check_logics[0]=="RepeaterLogic") {
-        // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
-        $ctcss = $svxconfig['RepeaterLogic']['REPORT_CTCSS'];
-        $system_type="IS_DUPLEX"; // if repeater
-        $dtmfctrl = $svxconfig['RepeaterLogic']['DTMF_CTRL_PTY']; 
-    }
-    if ($check_logics[0] =="SimplexLogic") {
-        // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
-        $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
-        $system_type = "IS_SIMPLEX"; // if simplex
-        $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY'];
-    }
-    
-}
-$_SESSION['system_type'] = $system_type;
-echo "\n$check_logics[0]";
+$screen = null;
+$retval = null;
 
-echo "\n$callsign";
-echo "\n$ctcss";
-echo "\n$system_type";
-echo "\n$_SESSION[system_type]";
+    exec('sudo cp /etc/svxlink/svxlink.conf /etc/svxlink/svxlink.conf.' .date("YmdThis") ,$screen,$retval);
+	//move generated file to current config
+    exec('sudo cp -f /etc/svxlink/svxlink.conf /var/www/html/svxlink/svxlink.conf', $screen, $retval);
+	exec('sudo mv /var/www/html/svxlink/svxlink.conf /etc/svxlink/svxlink.conf', $screen, $retval);
+        //	exec('sudo cp /etc/svxlink/svxlink.conf /etc/svxlink/svxlink.d/SomeLogic.conf', $screen, $retval);
+        //Service SVXlink restart
+    exec('sudo systemctl restart svxlink 2>&1',$screen,$retval);
+
+// debug
+      
+      
