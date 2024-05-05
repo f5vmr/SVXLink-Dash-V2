adeccab720ffa92706be5165b8c7b019429b2ccb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-05 09:59:21 +0100

    m

--- include/parse_svxconf.php
+++ include/parse_svxconf.php
@@ -29,6 +29,8 @@ foreach ($check_logics as $logic_key) {
     
 }
 $_SESSION['system_type'] = $system_type;
+var_dump($system_type);
+
 // additional variables need to define in svxlink.conf in stanza [ReflectorLogic]: API, FMNET, TG_URI
 // FMNET - Name of FM-Network
 // API - URI for access the status of SVXReflector you are connected
