547d23db90c6a65370072ad18850a3f6197d6365
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-05-15 14:28:38 +0100

    m

--- nodeInfo.php
+++ nodeInfo.php
@@ -59,9 +59,9 @@ include "include/settings.php";
 </div></center>
 </div>
 <?php
-//if (isProcessRunning('node')) {
-//echo '&nbsp;&nbsp;<button class="button link" onclick="playAudioToggle(8080, this)"><b>&nbsp;&nbsp;&nbsp;<img src=images/speaker.png alt="" style="vertical-align:middle">&nbsp;&nbsp;RX Monitor&nbsp;&nbsp;&nbsp;</b></button><br><br>';
-//}
+if (isProcessRunning('node')) {
+echo '&nbsp;&nbsp;<button class="button link" onclick="playAudioToggle(8080, this)"><b>&nbsp;&nbsp;&nbsp;<img src=images/speaker.png alt="" style="vertical-align:middle">&nbsp;&nbsp;RX Monitor&nbsp;&nbsp;&nbsp;</b></button><br><br>';
+}
 ?>
 <?php
 if (MENUBUTTON=="TOP") {
