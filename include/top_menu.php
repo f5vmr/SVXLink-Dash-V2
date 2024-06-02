853aa66e87189b864700e3615cea62c4b83cb347
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-02 12:38:40 +0100

    top menu update

--- include/top_menu.php
+++ include/top_menu.php
@@ -9,7 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
 	<a href="/index.php" style="color: #0000ff;">Dashboard</a> | 
 	<a href="/node.php" style="color: #0000ff;">Nodes</a> | 
 	<a href="/tg.php" style="color: #0000ff;">Talk Groups</a> | 
-	<a href="/dtmf.php" style="color: #0000ff;">Dtmf</a> | 
+	<!--<a href="/dtmf.php" style="color: #0000ff;">Dtmf</a> | --> 
 	<a href="/audio.php" style="color: #0000ff;">Audio</a> | 
 	<!--<a href="/wifi.php" style="color: #0000ff;">Wifi</a> | 
 	<a href="/network.php" style="color: #0000ff;">Network</a> |
