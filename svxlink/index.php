ca77161db8c1f9bdcaf38e2b8eb5292203f4c3b6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-15 16:30:51 +0100

    m

--- svxlink/index.php
+++ svxlink/index.php
@@ -107,8 +107,8 @@ foreach ($svxconfig as $section => $entries) {
         // Display the checkbox, key (command), and value (editable input)
         echo "<tr>";
         echo "<td style=width:35%> $key</td>";
-        echo "<td style=width:10%> input type='checkbox' name='active[$section][$key]' value='1' $checked></td>";
-        echo "<td style=width:45%> input type='text' name='value[$section][$key]' value='{$data['value']}'></td>";
+        echo "<td style=width:10% input type='checkbox' name='active[$section][$key]' value='1' $checked></td>";
+        echo "<td style=width:45% input type='text' name='value[$section][$key]' value='{$data['value']}'></td>";
         echo "</tr>\n";
     }
 }
