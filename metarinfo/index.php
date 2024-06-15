667456cfac7be5ece24741f02eaf73f756f75576
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-15 19:27:29 +0100

    m

--- metarinfo/index.php
+++ metarinfo/index.php
@@ -1,3 +1,11 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {       
+    session_start();
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -61,180 +69,83 @@ textarea {
 <fieldset style = "border:#3083b8 2px groove;box-shadow:5px 5px 20px #999; background-color:#f1f1f1; width:555px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
 <div style = "padding:0px;width:550px;background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);border-radius: 10px;-moz-border-radius:10px;-webkit-border-radius:10px;border: 1px solid LightGrey;margin-left:0px; margin-right:0px;margin-top:4px;margin-bottom:0px;line-height:1.6;white-space:normal;">
 <center>
-<h1 id="echolink" style = "color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">ModuleMetarInfo Configurator</h1>
+<h1 id="svxlink" style = "color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Metar Configurator</h1>
+<h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">To comment '#' a line, clear the checkbox</h3>
 
 
 <?php
-include_once "include/functions.php";
-$svxConfigFile = '/etc/svxlink/svxlink.conf';
-        if (fopen($svxConfigFile,'r'))
-              {
-                $svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
-                };
-                $logics = explode(",",$svxconfig['GLOBAL']['LOGICS']);
-                foreach ($logics as $key) {
-                  //echo "<tr><td style=\"background:#ffffed;\"><span style=\"color:#b5651d;font-weight: bold;\">".$key."</span></td></tr>";
-                 if ($key == "SimplexLogic") $isSimplex = true;
-                 if ($key == "RepeaterLogic") $isRepeater = true;
-                  }
 
-$miConfigFile = '/etc/svxlink/svxlink.d/ModuleMetarInfo.conf';
-if (fopen($miConfigFile,'r'))
-      {
-          $miconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
-      };
-  $module = str_split($miconfig['ModuleMetarInfo']);
-  foreach ($module as $key) {
-  if ($logics[0] == "[ModuleMetarInfo]") $isMetarInfo = true;
-  }
-
-if (isset($_POST['btnSave'])) {
-  $retval = null;
-  $screen = null;
-
-  $miconfig['ModuleMetarInfo']['MUTE_LOGIC_LINKING'] = $_POST['inMiMuteLogicLinking'];
-  $miconfig['ModuleMetarInfo']['SERVER'] = $_POST['inMiServer'];
-  $miconfig['ModuleMetarInfo']['LINK'] = $_POST['inMiLink'];
-  $miconfig['ModuleMetarInfo']['STARTDEFAULT'] = $_POST['inMiStartDefault'];
-  $miconfig['ModuleMetarInfo']['LONGMESSAGES'] = $_POST['inMiLongMessages'];
-  $miconfig['ModuleMetarInfo']['REMARKS'] = $_POST['inMiRemarks'];
-  $miconfig['ModuleMetarInfo']['AIRPORTS'] = $_POST['inmiAirports'];
-  $miconfig['ModuleMetarInfo']['MUTE_LOGIC_LINKING'] = $_POST['inElMuteLogicLinking'];
-
-  $ini = build_ini_string($miconfig);
-
-  //file_put_contents("/var/www/html/test.ini",$ini,FILE_USE_INCLUDE_PAT);
-  file_put_contents("/var/www/html/MetarInfo/ModuleMetarInfo.conf", $ini, FILE_USE_INCLUDE_PATH);
-
-  ///file manipulation section
-
-  $retval = null;
-  $screen = null;
-  //archive the current config
-  exec('sudo cp /etc/svxlink/svxlink.d/ModuleMetarInfo.conf /etc/svxlink/svxlink.d/ModuleMetarInfo.conf.' . date("YmdThis"), $screen, $retval);
-  //move generated file to current config
-  exec('sudo mv /var/www/html/echolink/ModuleMetarInfo.conf /etc/svxlink/svxlink.d/ModuleMetarInfo.conf', $screen, $retval);
-
-  //Service SVXlink restart
-  exec('sudo systemctl restart svxlink 2>&1', $screen, $retval);
+          
+        include_once "../include/functions.php";
+        $directory="/etc/svxlink/svxlink.d/";
+        $svxConfigFile = 'ModuelMetarInfo.conf';
+        $file = '/etc/svxlink/svxlink.d/ModuleMetarInfo.conf';
+        $owner = 'svxlink';
+        $group = 'svxlink';
 
 
 
-  //debug
-//      echo '<pre>';
-  //     print_r($ini);
-  //     echo '</pre>';
-//end of debug
+// Command to change ownership using sudo
+$command = "sudo chown $owner:$group " . escapeshellarg($file);
 
+// Execute the command
+$output = [];
+$return_var = 0;
+exec($command, $output, $return_var);
+
+// Check if the command executed successfully
+if ($return_var === 0) {
+ //   echo "Ownership of $file changed to $owner:$group successfully.";
+} else {
+//    echo "Failed to change ownership of $file. Error code: $return_var";
+ //   echo "<pre>" . implode("\n", $output) . "</pre>";
+}
+        file_backup($directory,$svxConfigFile);
+        $config = $directory.$svxConfigFile;
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+//
+if (file_exists($config)) {
+        $svxconfig = custom_parse_ini_file($config);
+    } else {
+        die("File not found: $config");
+    }
+    
+    // Debugging: Output parsed data
+    //var_dump($svxconfig);
+    
+    // Access and manipulate configuration
+  // Display form with checkboxes and editable values
+echo '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">';
+echo '<table>';
+echo '<tr><th>Command</th><th>Active</th><th>Value</th></tr>';
+
+foreach ($svxconfig as $section => $entries) {
+        echo "<tr><td colspan='3'><h2 id=\"svxlink\" style=\"color:#00aee8;font: 14pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;\">$section</h2></td></tr>\n";
+
+    foreach ($entries as $key => $data) {
+        // Determine checkbox state based on 'active' flag
+        $checked = $data['active'] ? 'checked' : '';
+        
+        // Display the checkbox, key (command), and value (editable input)
+        echo "<tr>";
+        echo "<td style='width: 15%'>$key</td>";
+        echo "<td style='width: 10%'><input type='checkbox' name='active[$section][$key]' value='1' $checked></td>";
+        echo "<td style='width: 75%'><input type='text' name='value[$section][$key]' style='width: 98%' value='{$data['value']}'></td>";
+        echo "</tr>\n";
+    }
 }
 
-
-if (fopen($miConfigFile,'r'))
-//      {
-
-        $miconfig = parse_ini_file($miConfigFile,true,INI_SCANNER_RAW);
-//};
-
-//$svxConfigFile = '/etc/svxlink/svxlink.conf';
-//$svxConfigFile = '/var/www/html/svxlink.conf';    
-
-
-
-
-
-
-//if (fopen($elConfigFile,'r'))
-  //    { 
-
-//	$elconfig = parse_ini_file($elConfigFile,true,INI_SCANNER_RAW);
+echo '</table>';
+echo '<button name="btnSave" type="submit" class="red" style = "height:100px; width:105px; font-size:12px;">Save <br><br> & <br><br> ReLoad</button>';
+echo '</form>'; 
+if (isset($_POST['btnSave'])) {
         
-	      $inMiMuteLogicLinking = $miconfig['ModuleMetarInfo']['MUTE_LOGIC_LINKING'];
-        $inMiServer = $miconfig['ModuleMetarInfo']['SERVER'];
-        $inMiLink = $miconfig['ModuleMetarInfo']['LINK'];
-        $inMiStartDefault = $miconfig['ModuleMetarInfo']['STARTDEFAULT'];
-        $inMiLongMessages = $miconfig['ModuleMetarInfo']['LONGMESSAGES'];
-        $inMiRemarks = $miconfig['ModuleMetarInfo']['REMARKS'];
-        $inmiAirports = $miconfig['ModuleMetarInfo']['AIRPORTS'];
+        save_svxconfig($config, $_POST);
+        $screen=null;
+        $retval=null;
+        exec('sudo systemctl restart svxlink 2>&1',$screen,$retval);
+    }
 
-//}
-//    else { $callsign="NOCALL";}
-
-
-
-//if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//  if (empty($_POST["ssid"])) {
-//     echo "Name is required";
-//  } else {
-//    $ssid = $_POST["ssid"]);
-//  }
-//}}
-
-
-// load the connlist
-$retval = null;
-$conns = null;
-// find the gateway
-//tbc - load the data from ini RF.
-
-?>
-
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-
-
-<table>
-        <tr>
-        <th width = "380px">MetarInfo Input</th>
-        <th width = "100px">Action</th>
-        </tr>
-<tr>
-<td>
-        <table style = "border-collapse: collapse; border: none;">
-        <tr style = "border: none;">
-                <th width = "30%"></th>
-                <th width = "70%"></th>
-        </tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Muting Logic Linking (advised 1)</td>
-        <td style = "border: none;"><input type="text" name="inMiMuteLogicLinking" style = "width:98%" value="<?php echo $inMiMuteLogicLinking;?>"></td>
-        </tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Metar Server</td>
-        <td style = "border: none;"><input type="text" name="inMiServer" style = "width:98%" value="<?php echo $inMiServer;?>">
-        </td></tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Link Text</td>
-        <td style = "border: none;"><input type="text" name="inMiLink" style = "width:98%" value="<?php echo $inMiLink;?>">
-        </td></tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Long Messages</td>
-        <td style = "border: none;"><input type="text" name="inMiLongMessages" style = "width:98%" value="<?php echo $inMiLongMessages;?>">
-        </td></tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Default Airport</td>
-        <td style = "border: none;"><input type="text" name="inMiStartDefault" style = "width:98%" value="<?php echo $inMiStartDefault;?>">
-        </td></tr>
-        <tr style = "border: none;"> 
-        <td style = "border: none;">Other Airports comma separated</td>
-        <td style = "border: none;"><input type="text" name="inMiAirports" style = "width:98%" value="<?php echo $inmiAirports;?>">
-        </td></tr>
-        </table>
-
-
-</TD>
-<td>
-        <button name="btnSave" type="submit" class="red" style = "height:100px; width:105px; font-size:12px;">Save <br><br> & <br><br> ReLoad</button>
-</td>
-
-</tr>
-</table>
-
-
-
-
-</form>
-
-<p style = "margin: 0 auto;"></p>
-<p style = "margin-bottom:-2px;"></p>
-
-</body>
-</html>
+ 
+    
