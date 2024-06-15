5ebad7f799e47ec78bfb3e9ba3b995bdc247e306
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-15 21:04:05 +0100

    m

--- amixer/index.php
+++ amixer/index.php
@@ -72,19 +72,25 @@ textarea {
 <h1 id="svxlink" style = "color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Audio Configurator</h1>
 <h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">AMixer settings</h3>
 <!-- HTML Form to adjust ALSA settings -->
-< method="post">
+
     
 <h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Loudspeaker - TX</h3>
-    <label for="loudspeaker">Loudspeaker (Transmitter Output) Set for 65 (0-100):</label>
+    <label for="loudspeaker">Set for 65 (0-100):</label>
     <input type="number" id="loudspeaker" name="loudspeaker" min="0" max="100" required>
 </br>
-    <label for="mic">Mic (0-100): Set to 0</label>
+<h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Loudspeaker - TX</h3>
+
+    <label for="mic">(0-100): Set to 0</label>
     <input type="number" id="mic" name="mic" min="0" max="100" required>
-</br>    
-    <label for="capture">Capture (0-100) Set for 25:</label>
+</br>
+<h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Microphone (Not used)</h3>
+
+    <label for="capture">(0-100) Set for 25:</label>
     <input type="number" id="capture" name="capture" min="0" max="100" required>
-</br>    
-    <label for="autogain">Auto Gain Control: Set to Off for optimum control</label>
+</br>
+<h3 style = "color:#00aee8;font: 12pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Audio Capture - RX</h3>
+
+    <label for="autogain">Set to Off for optimum control</label>
     <select id="autogain" name="autogain" required>
         <option value="off">Off</option>
         <option value="on">On</option>
