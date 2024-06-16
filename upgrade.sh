497f364107b5d8d07a4eabe6f4ea09dd9b624fe0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-06-16 15:10:47 +0100

    m

--- upgrade.sh
+++ upgrade.sh
@@ -64,3 +64,44 @@ find /var/www/html ! -name "$SCRIPT_FILE" -exec sudo chown svxlink:svxlink {} +
 
 # Inform the user that the ownership change was successful
 show_info "Ownership of files in /var/www/html has been changed to svxlink:svxlink, except for the script itself."
+
+# New section to create /home/pi/scripts and cleanup.sh
+SCRIPT_DIR="/home/pi/scripts"
+CLEANUP_SCRIPT="$SCRIPT_DIR/cleanup.sh"
+
+# Check if the script directory exists, if not, create it
+if [ ! -d "$SCRIPT_DIR" ]; then
+    mkdir -p "$SCRIPT_DIR"
+    show_info "Created directory $SCRIPT_DIR"
+fi
+
+# Check if the cleanup.sh script exists
+if [ -f "$CLEANUP_SCRIPT" ]; then
+    show_info "Script $CLEANUP_SCRIPT already exists. Exiting."
+    exit 0
+else
+    # Create the cleanup.sh script with the specified content
+    echo "#!/bin/bash
+
+# Directory to be cleaned
+DIR=\"/var/www/html/backups\"
+
+# Check if directory exists
+if [ -d \"\$DIR\" ]; then
+    # Find and delete files older than 7 days
+    find \"\$DIR\" -type f -mtime +7 -exec rm -f {} \;
+else
+    echo \"Directory \$DIR does not exist.\"
+fi" > "$CLEANUP_SCRIPT"
+
+    # Make the cleanup.sh script executable
+    sudo chmod +x "$CLEANUP_SCRIPT"
+    show_info "Created and made $CLEANUP_SCRIPT executable."
+fi
+
+# Check and add the cleanup.sh script to the sudo crontab if not already present
+CRON_JOB="01 00 * * * /home/pi/scripts/cleanup.sh"
+( sudo crontab -l | grep -q "$CRON_JOB" ) || ( sudo crontab -l; echo "$CRON_JOB" ) | sudo crontab -
+
+# Inform the user that the crontab entry has been added if it was not present
+show_info "Ensured that the crontab entry for $CLEANUP_SCRIPT exists."
