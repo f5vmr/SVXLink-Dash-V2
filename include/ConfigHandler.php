9df5245e903736ec3560e29a41eb451c33c33a5f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-01-29 15:42:34 +0000

    update

--- include/ConfigHandler.php
+++ include/ConfigHandler.php
@@ -1,8 +1,8 @@
 <?php
 class ConfigHandler {
     private static $instance = null;
-    private $reflectorConfig = null;
     private $mainConfig = null;
+    private $reflectorConfig = null;
     
     public static function getInstance() {
         if (self::$instance === null) {
@@ -16,10 +16,26 @@ class ConfigHandler {
     }
     
     private function loadConfigs() {
+        $mainFile = '/etc/svxlink/svxlink.conf';
         $reflectorFile = '/etc/svxlink/svxlink.d/ReflectorLogic.conf';
+        
+        if (file_exists($mainFile)) {
+            $this->mainConfig = parse_ini_file($mainFile, true, INI_SCANNER_RAW);
+        }
         if (file_exists($reflectorFile)) {
             $this->reflectorConfig = parse_ini_file($reflectorFile, true, INI_SCANNER_RAW);
         }
+    }
+    
+    public function getLogicModules() {
+        return $this->mainConfig['GLOBAL']['LOGICS'] ?? '';
+    }
+    
+    public function getActiveModules($logic) {
+        if (isset($this->mainConfig[$logic]['MODULES'])) {
+            return explode(",", str_replace('Module', '', $this->mainConfig[$logic]['MODULES']));
+        }
+        return [];
     }
     
     public function getCallsign() {
@@ -28,20 +44,5 @@ class ConfigHandler {
     
     public function getFmNetwork() {
         return $this->reflectorConfig['ReflectorLogic']['HOSTS'] ?? 'not registered';
-    }
-
-    public function getLogicModules() {
-        return $this->mainConfig['GLOBAL']['LOGICS'] ?? '';
-    }
-    public function getActiveModules($logic) {
-        $modules = [];
-        if (isset($this->mainConfig[$logic]['MODULES'])) {
-            $modules = explode(",", str_replace('Module', '', $this->mainConfig[$logic]['MODULES']));
-        }
-        return $modules;
-    }
-
-    public function getAuthKey() {
-        return $this->reflectorConfig['ReflectorLogic']['AUTH_KEY'] ?? '';
     }
 }
