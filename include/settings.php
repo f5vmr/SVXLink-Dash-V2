326d365fe694b06f8683671ed59c4f917b0d620e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-01-29 13:23:31 +0000

    update

--- include/settings.php
+++ include/settings.php
@@ -8,15 +8,21 @@ include_once "tools.php";
 include_once "functions.php";
 
 $svxConfigFile = '/etc/svxlink/svxlink.d/ReflectorLogic.conf';
-if (fopen($svxConfigFile, 'r')) {
+if (file_exists($svxConfigFile)) {
     $svxconfig = parse_ini_file($svxConfigFile, true, INI_SCANNER_RAW);
-    $callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
-    $fmnetwork = $svxconfig['ReflectorLogic']['HOSTS'];
-    //$tgUri = $svxconfig['ReflectorLogic']['TG_URI'];
-    $node_password = $svxconfig['ReflectorLogic']['AUTH_KEY'];
-    $node_user = $callsign;}
-else { $callsign="NOCALL"; 
-       $fmnetwork="not registered";
-	}
-
+    $_SESSION['callsign'] = $svxconfig['ReflectorLogic']['CALLSIGN'];
+    $_SESSION['fmnetwork'] = $svxconfig['ReflectorLogic']['HOSTS'];
+    $_SESSION['node_password'] = $svxconfig['ReflectorLogic']['AUTH_KEY'];
+    $_SESSION['node_user'] = $_SESSION['callsign'];
+    
+    $callsign = $_SESSION['callsign'];
+    $fmnetwork = $_SESSION['fmnetwork'];
+    $node_password = $_SESSION['node_password'];
+    $node_user = $_SESSION['node_user'];
+} else {
+    $_SESSION['callsign'] = "NOCALL";
+    $_SESSION['fmnetwork'] = "not registered";
+    $callsign = $_SESSION['callsign'];
+    $fmnetwork = $_SESSION['fmnetwork'];
+}
 ?>
