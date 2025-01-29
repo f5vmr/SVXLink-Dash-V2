49af6a09bd9c94d623015892ba2e5c4b86644f57
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-01-29 15:30:26 +0000

    update

--- authorise/index.php
+++ authorise/index.php
@@ -84,40 +84,33 @@ include_once "../include/config.php";
 
               
 
-
-
-
-
-
-
-                function checkAuth($username, $password) {
-                    if (session_status() === PHP_SESSION_NONE) {
-                        session_start();
+                function checkAuth($username, $password)
+                {
+                    if (session_status() == PHP_SESSION_NONE) {
+                        if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
                     }
                     // Check if received values match PHP_AUTH_USER and PHP_AUTH_PW
-
-
-                    if ($username === PHP_AUTH_USER && $password === PHP_AUTH_PW) {
+                    if ($username == PHP_AUTH_USER && $password == PHP_AUTH_PW) {
+                        // Success
                         $_SESSION['auth'] = "AUTHORISED";
                     } else {
                         $_SESSION['auth'] = "UNAUTHORISED";
                     }
-
+                    session_write_close();
                 }
 
                 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+                    include_once "../include/config.php";
                     // Process form submission
                     $username = isset($_POST['username']) ? $_POST['username'] : '';
                     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-    
                     checkAuth($username, $password);
 
-
-                    header("Location: ../index.php");
-                    exit();
-                }                ?>
+                }
+                ?>
                 <form method="POST">
                     Username:<input type="text" id="username" name="username" value="<?php echo $username; ?>"><br>
                     Password:<input type="password" id="password" name="password" value="<?php echo $password; ?>"><br>
