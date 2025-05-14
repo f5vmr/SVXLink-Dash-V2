3033aa6d00e98bb0dacd5d13cf459cd56d1ac053
DL3EL <dl3el@darc.de>
2025-05-14 11:20:41 +0200

    disable rx-monitor
    Signed-off-by: DL3EL <dl3el@darc.de>

--- editor.php
+++ editor.php
@@ -68,11 +68,6 @@ include_once "include/top_menu.php";
 </div></center>
 </div>
 <?php
-if (isProcessRunning('node')) {
-echo '&nbsp;&nbsp;<button class="button link" onclick="playAudioToggle(8080, this)"><b>&nbsp;&nbsp;&nbsp;<img src=images/speaker.png alt="" style = "vertical-align:middle">&nbsp;&nbsp;RX Monitor&nbsp;&nbsp;&nbsp;</b></button><br><br>';
-}
-?>
-<?php
 if (MENUBUTTON=="TOP") {
 include_once "include/buttons.php";
 
