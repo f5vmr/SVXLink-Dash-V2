27c82f8f52c64f7e8be520f82fb10424e7688eb4
root <dl3el@darc.de>
2025-06-25 14:50:17 +0200

    make radio status directory independant
    Signed-off-by: root <dl3el@darc.de>

--- dl3el/sa818/get_shari_hf_data.pl
+++ dl3el/sa818/get_shari_hf_data.pl
@@ -1,17 +1,31 @@
 #!/usr/bin/perl
 use v5.28;
 
+use Time::Piece;
+
 my $verbose = 0;
 my $cmd = "";
 my $data = "";
 
+    my $log_time = act_time();
 
-#    $cmd = sprintf("sudo python3 /home/pi/SVXLink/shari/sa818-running.py -q");
-    $cmd = sprintf("python3 /home/svxlink/sa818-running.py -q");
-    print "[$cmd]\n" if ($verbose >= 1);
-#    `$cmd`;
+	my $cmd = "pwd";
+	my $dir =`$cmd`;
+	my $dirr = trim_cr($dir);
+    $dir = ($dirr =~ /(.*)\/include/s)? $1 : "undef";
+
+    if ($dir eq "undef") {
+        $dir = $dirr;
+    }    
+
+    print "[$log_time] DIR $dir ($dirr)\n" if ($verbose >= 0);
+	my $shari = $dir  . "/dl3el/sa818";
+    print "[$log_time] SHARI $shari\n" if ($verbose >= 0);
+
+    $cmd = sprintf("python3 %s/sa818-running.py -q",$shari);
+    print "[$log_time] [$cmd]\n" if ($verbose >= 1);
 	$cmd = qx($cmd);
-    print "[$cmd]\n" if ($verbose >= 1);
+    print "[$log_time] [$cmd]\n" if ($verbose >= 1);
 # +DMOREADGROUP:1,430.5750,430.5750,0000,1,0002
     $data = ($cmd =~ /.*\+DMOREADGROUP:(\d),([\d-|\.]+),([\d-|\.]+),([\w]+),(\d),([\w]+)/s)? $1 : "undef";
     printf "%s / %s",$2,$6;
@@ -23,3 +37,15 @@ exit 0;
     print "TXCTCSS: [$4]\n" if ($verbose >= 0);
     print "Squelch: [$5]\n" if ($verbose >= 0);
     print "RXCTCSS: [$6]\n" if ($verbose >= 0);
+
+sub trim_cr {
+	my $string = $_[0];
+	$string = shift;
+	$string =~ s/\n//g;
+	return $string;
+}
+
+sub act_time {
+	my $tm = localtime(time);
+	return (sprintf("%02d:%02d:%02d",$tm->hour, $tm->min, $tm->sec));
+}
