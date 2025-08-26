bcd872a68a50034b86def5d92d60cb69c330a40f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-08-26 12:17:24 +0100

    update

--- upgrade.sh
+++ upgrade.sh
@@ -64,6 +64,7 @@ find /var/www/html ! -name "$SCRIPT_FILE" -exec sudo chown svxlink:svxlink {} +
 # Change ownership of all files in /etc/svxlink and sub directories.
 find /etc/svxlink -type f -exec sudo chown svxlink:svxlink {} +
 # Set DTMF active.
+sudo mkdir -p /var/run/svxlink
 sudo touch /var/run/svxlink/dtmf_svx
 sudo chown svxlink:svxlink /var/run/svxlink/dtmf_svx
 sudo chmod 775 /var/run/svxlink/dtmf_svx
