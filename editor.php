fbebde0b5053ba4901cf4c34e62aad29708c6c50
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-03 13:17:41 +0000

    update form

--- editor.php
+++ editor.php
@@ -77,18 +77,20 @@ include_once "include/top_menu.php";
 </div>
 <?php
 $nodeRunning = isProcessRunning('node');
+
 if ($nodeRunning) {
-    echo '<div style="display:inline-block; text-align:center;">';
-    echo '<button class="button link" onclick="playAudioToggle(8001, this)">
-        <b><img src="images/speaker.png" alt="" style="vertical-align:middle">&nbsp;RX Monitor</b>
-    </button><br>';
-    echo '<span id="listenerCount" style="font-weight:bold; margin-top:5px; display:block;"></span>';
-    echo '</div>';
+    echo '<button class="button link" onclick="playAudioToggle(8001, this)" style="position: relative; display: inline-block; text-align: center;">
+            <b><img src="images/speaker.png" alt="" style="vertical-align:middle">&nbsp;RX Monitor</b>
+            <div id="listenerCount" style="
+                font-weight: bold;
+                font-size: 0.85em;
+                margin-top: 4px;
+            "></div>
+          </button>';
 } else {
     echo 'Node process not detected';
 }
 ?>
-
 
 
 <script>
