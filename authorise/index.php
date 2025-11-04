4c2fa867f17871531998bb892c553d3f9b7fbb93
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-04 13:54:28 +0000

    update.authorise

--- authorise/index.php
+++ authorise/index.php
@@ -23,101 +23,55 @@ include_once "../include/config.php";
             color: #444;
             padding: 0 0.5em;
         }
-
-        h1, h2, h3 {
-            line-height: 1.2;
+        h1 { line-height: 1.2; }
+        fieldset {
+            border:#3083b8 2px groove;
+            box-shadow:5px 5px 20px #999;
+            background-color:#f1f1f1;
+            width:555px;
+            margin: 15px auto 0 auto;
+            font-size:13px;
+            border-radius: 10px;
         }
-
-        a {
-            color: #607d8b;
+        .container {
+            padding: 0.5em;
+            background: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);
+            border-radius: 10px;
+            border: 1px solid LightGrey;
+            text-align: center;
         }
-
-        .highlighter-rouge {
-            background-color: #fff;
-            border: 1px solid #ccc;
-            border-radius: .2em;
-            font-size: .8em;
-            overflow-x: auto;
-            padding: .2em .4em;
+        input[type="text"], input[type="password"] {
+            font-size: 14px;
+            padding: 4px;
+            margin: 4px 0;
         }
-
-        pre {
-            margin: 0;
-            padding: .6em;
-            overflow-x: auto;
-        }
-
-        #player {
-            position: relative;
-            width: 205px;
-            overflow: hidden;
-            direction: ltl;
-        }
-
-        textarea {
-            background-color: #111;
-            border: 1px solid #000;
-            color: #ffffff;
-            padding: 1px;
-            font-family: courier new;
-            font-size: 10px;
+        input[type="submit"] {
+            font-size: 14px;
+            padding: 4px 10px;
         }
     </style>
 </head>
-
-<body style = "background-color: #e1e1e1;font: 11pt arial, sans-serif;">
+<body>
 <center>
-    <fieldset
-            style = "border:#3083b8 2px groove;box-shadow:5px 5px 20px #999; background-color:#f1f1f1; width:555px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
-        <div
-                style = "padding:0px;width:550px;background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);border-radius: 10px;-moz-border-radius:10px;-webkit-border-radius:10px;border: 1px solid LightGrey;margin-left:0px; margin-right:0px;margin-top:4px;margin-bottom:0px;line-height:1.6;white-space:normal;">
-            <center>
-                <?php
-                if ($_SESSION['auth'] === 'AUTHORISED') {
-                    ?>
-                    <h1 id="authorise" style = "color:#00ff00;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Authorised</h1>
-                <?php } else { ?>
-                    <h1 id="authorise" style = "color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Authorise</h1>
+    <fieldset>
+        <div class="container">
+            <?php if ($_SESSION['auth'] === 'AUTHORISED') { ?>
+                <h1 style="color:#00ff00;font-weight:bold;">Authorised</h1>
+            <?php } else { ?>
+                <h1 style="color:#00aee8;font-weight:bold;">Authorise</h1>
+                <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') { ?>
+                    <p style="color:red;">Incorrect username or password.</p>
                 <?php } ?>
-
-                <?php
-
-              
-
-                function checkAuth($username, $password)
-                {
-                    if (session_status() == PHP_SESSION_NONE) {
-                        if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-                    }
-                    // Check if received values match PHP_AUTH_USER and PHP_AUTH_PW
-                    if ($username == PHP_AUTH_USER && $password == PHP_AUTH_PW) {
-                        // Success
-                        $_SESSION['auth'] = "AUTHORISED";
-                    } else {
-                        $_SESSION['auth'] = "UNAUTHORISED";
-                    }
-                    session_write_close();
-                }
-
-                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                    include_once "../include/config.php";
-                    // Process form submission
-                    $username = isset($_POST['username']) ? $_POST['username'] : '';
-                    $password = isset($_POST['password']) ? $_POST['password'] : '';
-
-                    checkAuth($username, $password);
-
-                }
-                ?>
                 <form method="POST">
-                    Username:<input type="text" id="username" name="username" value="<?php echo $username; ?>"><br>
-                    Password:<input type="password" id="password" name="password" value="<?php echo $password; ?>"><br>
+                    Username:<br>
+                    <input type="text" name="username" required><br>
+                    Password:<br>
+                    <input type="password" name="password" required><br>
                     <input type="submit" value="Submit">
                 </form>
-            </center>
+            <?php } ?>
         </div>
     </fieldset>
+</center>
 </body>
 </html>
