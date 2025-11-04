7941982eb01d9312a1030b1b02c738bb535a9a61
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-04 11:30:03 +0000

    remove debugging

--- include/tools.php
+++ include/tools.php
@@ -92,9 +92,3 @@ function cidr_match($ip, $cidr) {
     }
     return $outcome;
 }
-function debug_log($message) {
-  // Log to file
-  file_put_contents('/var/www/html/include/buttons_debug.log', date('[Y-m-d H:i:s] ') . $message . PHP_EOL, FILE_APPEND);
-  // Also display on screen if needed
-  //echo "<div style='background-color:#ffeeee;border:1px solid red;padding:5px;margin:5px;'><pre>DEBUG: " . htmlspecialchars($message) . "</pre></div>";
-}
