fed5b9d296005f5c9c6acf3bb32c96f131fe99dd
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-07 09:11:04 +0000

    update field size

--- log/index.php
+++ log/index.php
@@ -125,13 +125,38 @@ window.onload = fetchLog;
 </head>
 <body style="background-color: #e1e1e1;font: 11pt arial, sans-serif;">
 <center>
-    <fieldset style="border:#3083b8 2px groove;box-shadow:5px 5px 20px #999; background-color:#f1f1f1; width:560px;margin-top:10px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
-        <div style="padding:0px;width:560px;background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);border-radius: 10px;-moz-border-radius:10px;-webkit-border-radius:10px;border: 1px solid LightGrey;margin-left:0px; margin-right:0px;margin-top:4px;margin-bottom:0px;line-height:1.4;white-space:normal;">
-            <h1 id="web-audio-peak-meters" style="color:#00aee8;font: 16pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Live Log Viewer</h1>
-            <pre id="log" style="text-align: left;">Loading log...</pre>
+    <fieldset style="border:#3083b8 2px groove;
+                 box-shadow:5px 5px 20px #999; 
+                 background-color:#f1f1f1; 
+                 width:565px;              /* +5px */
+                 margin-top:5px;           /* push up a bit */
+                 margin-left:-5px;         /* push left 5px */
+                 margin-right:5px;         /* right stays */
+                 font-size:13px;
+                 border-radius: 10px;">
+    <div style="padding:0px;
+                width:565px;            /* +5px */
+                background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);
+                border-radius: 10px;
+                -moz-border-radius:10px;
+                -webkit-border-radius:10px;
+                border: 1px solid LightGrey;
+                margin-left:-5px;       /* push left */
+                margin-right:-5px;      /* push right */
+                margin-top:-1px;        /* push upward */
+                margin-bottom:-1px;     /* push downward */
+                line-height:1.4;
+                white-space:normal;">
+        <h1 id="web-audio-peak-meters" style="color:#00aee8;
+                                             font:16pt arial, sans-serif;
+                                             font-weight:bold; 
+                                             text-shadow:0.25px 0.25px gray;">
+            Live Log Viewer
+        </h1>
+        <pre id="log" style="text-align:left;">Loading log...</pre>
+    </div>
+</fieldset>
 
-        </div>
-    </fieldset>
 </center>
 <p style="margin: 0 auto;"></p>
 <p style="margin-bottom:-2px;"></p>
