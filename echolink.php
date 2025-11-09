d27b99528cb4a30ef5881fe07b30327319dc5697
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-09 10:16:41 +0000

    colour change

--- echolink.php
+++ echolink.php
@@ -46,9 +46,9 @@ include "include/settings.php";
     <div class="img" style = "padding-left:30px"><img src="images/svxlink.ico" /></div>
     <div class="text"style = "padding-right:230px">
 <center><p style = "margin-top:5px;margin-bottom:0px;">
-<span style = "font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#6495ED"><?php echo $callsign; ?></span>
+<span style = "font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#00ffff"><?php echo $callsign; ?></span>
 <p style = "margin-top:0px;margin-bottom:0px;">
-<span style = "font-size: 18px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#6495ED"><?php echo $fmnetwork; ?></span>
+<span style = "font-size: 18px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#00ffff"><?php echo $fmnetwork; ?></span>
 </p></center>
 </div></div>
 </div>
