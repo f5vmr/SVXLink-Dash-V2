590f1f1ad9edbc6b220e80d4eecfcdabf5f63125
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-10 15:52:46 +0000

    update

--- include/buttons.php
+++ include/buttons.php
@@ -1,7 +1,7 @@
 
 <?php
-include_once __DIR__ . "/include/tools.php";
-include_once __DIR__ . "/include/config.buttons.php";
+include_once "tools.php";
+include_once "config.buttons.php";
 //include_once __DIR__ . "/../dtmf.php";
 ?>
 
