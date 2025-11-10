2f3c5c19a9f1e5bd6dd3d5270060988be78913b8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-10 14:57:44 +0000

    update

--- buttons/index.php
+++ buttons/index.php
@@ -2,6 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+include_once __DIR__ . '/../include/config.buttons.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- include/config.inc.php
+++ include/config.inc.php
@@ -1,5 +1,6 @@
 <?php
 include_once __DIR__."/config.buttons.php";
+
 // Path to the authentication file
 $auth_file = '/etc/svxlink/dashboard.auth.ini';
 
