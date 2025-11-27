ee3e71280c40c536ed6153ed51a83f37456a7c6a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-27 11:48:30 +0000

    revert

--- nodeInfo/index.php
+++ nodeInfo/index.php
@@ -81,73 +81,46 @@ if (session_status() === PHP_SESSION_NONE) {
                 return isset($_POST[$key]) ? htmlspecialchars($_POST[$key], ENT_QUOTES, 'UTF-8') : htmlspecialchars($default, ENT_QUOTES, 'UTF-8');
             }
 
-            // ----------- btnSave routine -----------
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSave'])) {
-
-                // Read existing JSON exactly as it is
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                // Read the file content again
                 $json_content = file_get_contents($filename);
                 $data = json_decode($json_content, true);
 
-                // Helper to get raw POST input without escaping
-                function raw_post($key, $default = '') {
-                    return isset($_POST[$key]) ? $_POST[$key] : $default;
-                }
+                // Update the data with posted values
+                $data['nodeLocation'] = get_form_data('nodeLocation', $data['nodeLocation']);
+                $data['hidden'] = isset($_POST['hidden']) ? true : false;
+                $data['sysop'] = get_form_data('sysop', $data['sysop']);
 
-                // Update fields
-                $data['nodeLocation'] = raw_post('nodeLocation', $data['nodeLocation']);
-                $data['hidden']       = isset($_POST['hidden']);
-                $data['sysop']        = raw_post('sysop', $data['sysop']);
+                $data['qth'][0]['name'] = get_form_data('qth_name', $data['qth'][0]['name']);
+                $data['qth'][0]['pos']['lat'] = get_form_data('qth_lat', $data['qth'][0]['pos']['lat']);
+                $data['qth'][0]['pos']['long'] = get_form_data('qth_long', $data['qth'][0]['pos']['long']);
+                $data['qth'][0]['pos']['loc'] = get_form_data('qth_loc', $data['qth'][0]['pos']['loc']);
+                $data['qth'][0]['rx']['K']['name'] = get_form_data('rx_name', $data['qth'][0]['rx']['K']['name']);
+                $data['qth'][0]['rx']['K']['freq'] = filter_input(INPUT_POST, 'rx_freq', FILTER_VALIDATE_FLOAT, ['options' => ['default' => $data['qth'][0]['rx']['K']['freq']]]);
+                $data['qth'][0]['rx']['K']['sqlType'] = get_form_data('rx_sqlType', $data['qth'][0]['rx']['K']['sqlType']);
+                $data['qth'][0]['tx']['K']['name'] = get_form_data('tx_name', $data['qth'][0]['tx']['K']['name']);
+                $data['qth'][0]['tx']['K']['freq'] = filter_input(INPUT_POST, 'tx_freq', FILTER_VALIDATE_FLOAT, ['options' => ['default' => $data['qth'][0]['tx']['K']['freq']]]);
+                $data['qth'][0]['tx']['K']['pwr'] = get_form_data('tx_pwr', $data['qth'][0]['tx']['K']['pwr']);
 
-                $data['qth'][0]['name']         = raw_post('qth_name', $data['qth'][0]['name']);
-                $data['qth'][0]['pos']['lat']   = raw_post('qth_lat', $data['qth'][0]['pos']['lat']);
-                $data['qth'][0]['pos']['long']  = raw_post('qth_long', $data['qth'][0]['pos']['long']);
-                $data['qth'][0]['pos']['loc']   = raw_post('qth_loc', $data['qth'][0]['pos']['loc']);
-
-                $data['qth'][0]['rx']['K']['name']    = raw_post('rx_name', $data['qth'][0]['rx']['K']['name']);
-                $data['qth'][0]['rx']['K']['freq']    = raw_post('rx_freq', $data['qth'][0]['rx']['K']['freq']);
-                $data['qth'][0]['rx']['K']['sqlType'] = raw_post('rx_sqlType', $data['qth'][0]['rx']['K']['sqlType']);
-
-                $data['qth'][0]['tx']['K']['name'] = raw_post('tx_name', $data['qth'][0]['tx']['K']['name']);
-                $data['qth'][0]['tx']['K']['freq'] = raw_post('tx_freq', $data['qth'][0]['tx']['K']['freq']);
-                $data['qth'][0]['tx']['K']['pwr']  = raw_post('tx_pwr', $data['qth'][0]['tx']['K']['pwr']);
-
-                // Convert updated array back to JSON
+                // Encode the updated array back to JSON
                 $new_json_content = json_encode($data, JSON_PRETTY_PRINT);
 
-                // Create backup
+                // Create a backup
                 $backup_filename = $backup_dir . 'node_info_backup_' . date('YmdHis') . '.json';
-                if (!copy($filename, $backup_filename)) {
-                    echo "Error creating backup file.";
-                    exit;
-                }
-
-                // Write updated JSON back to main file
-                if (file_put_contents($filename, $new_json_content) === false) {
-                    echo "Error saving node_info.json";
-                    exit;
-                }
-
-                // Give filesystem a moment
-                sleep(1);
-
-                // Restart SVXLink
-                exec('sudo systemctl restart svxlink 2>&1', $screen, $retval);
-
-                if ($retval === 0) {
-                    echo "<script type='text/javascript'>reloadPage();</script>";
-                    exit;
+                if (copy($filename, $backup_filename)) {
+                    if (file_put_contents($filename, $new_json_content) === false) {
+                        $message = 'Error saving file';
+                    } else {
+                        $message = 'File saved successfully';
+                    }
                 } else {
-                    echo "Failed to restart SVXLink.<br>";
-                    echo nl2br(htmlspecialchars(implode("\n", $screen)));
-                    exit;
+                    $message = 'Error creating backup file';
                 }
             } else {
-                // Read the file content for display
+                // Read the file content
                 $json_content = file_get_contents($filename);
                 $data = json_decode($json_content, true);
             }
-            // ----------- end btnSave routine -----------
-
             ?>
             <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
                 <table>
@@ -180,8 +153,97 @@ if (session_status() === PHP_SESSION_NONE) {
                             <button name="btnSave" type="submit" class="red" style="height:100px; width:105px; font-size:12px;">Save <br> & <br> ReLoad</button>
                         </td>
                     </tr>
-                    <!-- QTH, Receiver, Transmitter sections remain unchanged -->
-                    <!-- (same as your original HTML, omitted for brevity) -->
+                    <tr>
+                        <th width="380px">QTH Information</th>
+                        <th width="100px">Action</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table style="border-collapse: collapse; border: none;">
+                                <tr style="border: none;">
+                                    <th width="30%"></th>
+                                    <th width="70%"></th>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Name:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="qth_name" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['name']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Latitude:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="qth_lat" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['pos']['lat']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Longitude:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="qth_long" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['pos']['long']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Locator:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="qth_loc" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['pos']['loc']); ?>"></td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <button name="btnSave" type="submit" class="red" style="height:100px; width:105px; font-size:12px;">Save <br> & <br> ReLoad</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th width="380px">Receiver Information</th>
+                        <th width="100px">Action</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table style="border-collapse: collapse; border: none;">
+                                <tr style="border: none;">
+                                    <th width="30%"></th>
+                                    <th width="70%"></th>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Name:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="rx_name" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['rx']['K']['name']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Frequency:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="rx_freq" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['rx']['K']['freq']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">SQL Type:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="rx_sqlType" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['rx']['K']['sqlType']); ?>"></td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <button name="btnSave" type="submit" class="red" style="height:100px; width:105px; font-size:12px;">Save <br> & <br> ReLoad</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th width="380px">Transmitter Information</th>
+                        <th width="100px">Action</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table style="border-collapse: collapse; border: none;">
+                                <tr style="border: none;">
+                                    <th width="30%"></th>
+                                    <th width="70%"></th>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Name:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="tx_name" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['tx']['K']['name']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Frequency:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="tx_freq" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['tx']['K']['freq']); ?>"></td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none;">Power:</td>
+                                    <td style="border: none;"><input type="text" class="form-control" name="tx_pwr" style="width:98%" value="<?php echo htmlspecialchars($data['qth'][0]['tx']['K']['pwr']); ?>"></td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <button name="btnSave" type="submit" class="red" style="height:100px; width:105px; font-size:12px;">Save <br> & <br> ReLoad</button>
+                        </td>
+                    </tr>
                 </table>
             </form>
 
@@ -192,3 +254,7 @@ if (session_status() === PHP_SESSION_NONE) {
     </center>
 </body>
 </html>
+
+
+
+
