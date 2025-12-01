3881b21bcea8c8be45f340391d1514ed5f46ed43
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-12-01 20:45:17 +0000

    update

--- talkgroups/index.php
+++ talkgroups/index.php
@@ -39,7 +39,7 @@ if (isset($_POST['btnSave'])) {
         <div style="padding:10px; width:100%; background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%); border-radius:10px; border:1px solid LightGrey; white-space:normal; box-sizing:border-box;">
 
             <form method="POST" style="text-align:center;">
-                <h1 id="svxlink" style="color:#e8e8e8e8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Reflector Talkgroup Settings</h1>
+                <h1 id="svxlink" style="color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Reflector Talkgroup Settings</h1>
 
                 <?php
                 // Single hints block
