80443cf0c560b5ad5e01b786a99d44a424a2a224
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-12-01 12:40:55 +0000

    update

--- talkgroups/index.php
+++ talkgroups/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Include parser and config if needed
 include_once __DIR__ . "/tools.php";
 include_once __DIR__ . "/config.talkgroups.php";
