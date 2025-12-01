0d83cf323a2c10476e5adb29823f2c0dab9fef77
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-12-01 21:24:31 +0000

    update

--- include/talkgroups.php
+++ include/talkgroups.php
@@ -75,7 +75,8 @@ function updateTalkgroups($default_tg, $monitoring_array) {
  * Render the input boxes for DEFAULT_TG and MONITOR_TGS
  */
 function renderTalkgroupInputs($default_tg, $monitor_tgs) {
-    $html = "<table style='margin:auto; text-align:center;'>";
+    $html = "<table style='margin:auto; text-align:center; width:520px;'>";
+
 
     // DEFAULT_TG single box
     $html .= "<tr><td>Default TG:</td>";
