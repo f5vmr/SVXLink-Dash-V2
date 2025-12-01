b9427aaa9ef1131af3e30c53d474efc0c043740f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-12-01 12:20:26 +0000

    update

--- talkgroups/index.php
+++ talkgroups/index.php
@@ -2,7 +2,10 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-include_once __DIR__ . "/../../include/talkgroups.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include_once __DIR__ . '/../include/talkgroups.php;
 
 // Load current values
 $default_tg = getDefaultTG();
